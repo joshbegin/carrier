27cb0a29eed2838215b4eb91666210b9c15d65b3
Josh Begin <joshbegin@gmail.com>
2012-09-25 20:24:55 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carrier::Application.config.secret_token = '293518f753279b7e65caf3e83cb453ee8731f7aff5f9cc17769a784b6aa13deacd731604ce470c52b63874cdae164b8aeab1ea11b78f519878f54fc3ec96c5ec'
